8f77be06346cfb80e7ac6fd4c740ceebee669ed2
bran.nie <pengcheng1995@gmail.com>
2020-07-20 18:18:44 +0800

    重新学习react，1-7

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function AppHeader(props) {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
+    );
+}
+
+function Welcome(props) {
+    const { name } = props;
+    return <h1>Hello, {name}</h1>;
+}
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date(), interval: 0 };
+    }
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    tick() {
+        this.setState((state, props) => {
+            // console.log(state, props);
+            return {
+                date: new Date(),
+            };
+        });
+    }
+    render() {
+        return (
+            <div>
+                {this.props?.name} It is {this.state.date.toLocaleTimeString()}.
+            </div>
+        );
+    }
+}
+
+const showHeader = false;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {showHeader && <AppHeader />}
+            <Welcome name="Bran" />
+            <Welcome name="David" />
+            <Clock />
+            <Clock name="clock2" />
+        </div>
+    );
 }
 
 export default App;
